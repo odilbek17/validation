d88a878f7802eecb27dceb4fec286b2affa094eb
Odilbek G'ulomov <gulomovodilbek430@gmail.com>
2025-07-26 16:14:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const app=express()
 mongoose
   .connect(
-    "mongodb+srv://shohzodganiyevich:rYfdNinLCy9xTAqy@cluster0.nlr3uyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://gulomovodilbek430:<dbkJsBX4hIlq6YUXEy>@cluster0.o7uz65r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("connected to db"))
   .catch((e) => console.log(e));
 
 app.use(express.json())
+kJsBX4hIlq6YUXEy
